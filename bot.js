e0b3e4a8b1f960e6b6efc9a6ccbb3c0e62e0e7c4
Gebeyaw21 <140742517+Gebeyaw21@users.noreply.github.com>
2026-08-21 17:19:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,21 @@
 
+const TelegramBot = require('node-telegram-bot-api');
+// እባክዎ የቦትዎን ቶከን እዚህ ያስገቡ
+const token = 'YOUR_BOT_TOKEN_HERE'; 
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, "እንኳን ወደ አፍሮ ቢንጎ በደህና መጡ!", {
+        reply_markup: {
+            keyboard: [
+                [{ text: '🎮 Play Game' }],
+                [{ text: '💰 Deposit' }, { text: '💸 Withdraw' }],
+                [{ text: '💳 Balance' }],
+                [{ text: '📞 Support' }, { text: '🎁 Invite Friends' }]
+            ],
+            resize_keyboard: true,
+            is_persistent: true
+        }
+    });
+});
